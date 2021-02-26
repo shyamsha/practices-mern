1c88aafb6cc95e75db3103187f14517b2ee53400
sHyaMsha <shyamkumar97@live.com>
2021-02-26 16:58:03 +0530

    reformat

--- practice.js
+++ practice.js
@@ -1,53 +1,53 @@
-// // variable decleration
-// let i = 1;
-// //the name shoud beign with alpha letter or underscore or doller symbal
-// let j = 'string'
-//     // before the time variable declere by var
-// var o = '25'
-//     // constant varible it is fixed can't be changed
-// const h = 52
-// i = 2
-// console.log(i, j, o, h)
-//     // primitive type data types
-//     //1.number
-// let num = 25
-// console.log(typeof num)
-//     //methods
-// let m = Math.PI
-// console.log(typeof m);
-// console.log(Math.PI); //property
-// console.log(Math.abs(-2))
-// console.log(Math.ceil(2.1))
-// console.log(Math.floor(2.9))
-// console.log(Math.sqrt(2))
-// console.log(Math.pow(2, 2))
-// console.log(Math.round(2.45641))
-// console.log(Math.min(0, 1, 2))
-// console.log(Math.max(99, 999, 322))
-// console.log(Math.floor(Math.random() * 5))
-// console.log(typeof o);
+// variable decleration
+let i = 1;
+//the name shoud beign with alpha letter or underscore or doller symbal
+let j = 'string'
+    // before the time variable declere by var
+var o = '25'
+    // constant varible it is fixed can't be changed
+const h = 52
+i = 2
+console.log(i, j, o, h)
+    // primitive type data types
+    //1.number
+let num = 25
+console.log(typeof num)
+    //methods
+let m = Math.PI
+console.log(typeof m);
+console.log(Math.PI); //property
+console.log(Math.abs(-2))
+console.log(Math.ceil(2.1))
+console.log(Math.floor(2.9))
+console.log(Math.sqrt(2))
+console.log(Math.pow(2, 2))
+console.log(Math.round(2.45641))
+console.log(Math.min(0, 1, 2))
+console.log(Math.max(99, 999, 322))
+console.log(Math.floor(Math.random() * 5))
+console.log(typeof o);
 
-// //type conservations
-// let k = NaN;
-// console.log('f' + 5); //string concatanation anything add to string 
-// console.log(5 + '5');
-// console.log(String(h));
-// console.log(parseFloat(19.25));
-// console.log(parseInt(19.2562));
-// console.log(Number.isNaN(''));
-// console.log(Number.isNaN(k));
-// console.log(Number('25 vb'));
-// console.log(new Date());
-// console.log(5 + 5);
-// console.log(5 + 6 + '5');
-// console.log('65' + 5 + 5);
-// //null is assigned value , its means nothing
-// //undefind mean variable is declare but not defind
-// //null is an object undefind is type of undefined
-// //null is equl to undefined
-// console.log(typeof null);
-// console.log(null != undefined);
-// console.log(null == undefined);
+//type conservations
+let k = NaN;
+console.log('f' + 5); //string concatanation anything add to string 
+console.log(5 + '5');
+console.log(String(h));
+console.log(parseFloat(19.25));
+console.log(parseInt(19.2562));
+console.log(Number.isNaN(''));
+console.log(Number.isNaN(k));
+console.log(Number('25 vb'));
+console.log(new Date());
+console.log(5 + 5);
+console.log(5 + 6 + '5');
+console.log('65' + 5 + 5);
+//null is assigned value , its means nothing
+//undefind mean variable is declare but not defind
+//null is an object undefind is type of undefined
+//null is equl to undefined
+console.log(typeof null);
+console.log(null != undefined);
+console.log(null == undefined);
 var x = true,
     y = false
 var str = 'sHyaM cherukuri"s'
